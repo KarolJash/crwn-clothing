485c78ad0d6f0ff1f1360d3ebd5c52c0e90bdb37
KarolJash <karojl6309@gmail.com>
2020-08-25 16:21:43 -0400

    added addItem and removeItem functionality to ckeckout item component

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,6 +1,7 @@
 import CartActionTypes from './cart.types'
 import {
-    addItemToCart
+    addItemToCart,
+    removeItemFromCart
 } from './cart.utils'
 
 const INITIALE_STATE = {
@@ -19,6 +20,18 @@ const cartReducer = (state = INITIALE_STATE, action) => {
             return {
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload)
+            };
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(
+                    cartItem => cartItem.id !== action.payload.id
+                )
+            };
+        case CartActionTypes.REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
             };
         default:
             return state
